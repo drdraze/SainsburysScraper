91cb8c150aaccf7c19a2db605a5a111854ca6ae2
Ben Drazen <ben.drazen@gmail.com>
2019-05-05 19:32:24 +0100

    Json build code added
    Code that builds Json added to main sainsburys scraper class.

--- src/org/bend/scraper/SainsburysScraper.java
+++ src/org/bend/scraper/SainsburysScraper.java
@@ -1,5 +1,8 @@
 package org.bend.scraper;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -8,14 +11,31 @@ public class SainsburysScraper {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Page page = new Page ("https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html","productLister gridView");
+		Page page = new Page ("https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html","gridItem");
 		page.init();
-		GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        
-        System.out.println(gson.toJson(page.getProductList()));
-         
 		
+		GsonBuilder builder = new GsonBuilder();
+        builder.disableHtmlEscaping();
+        Gson gson = builder.setPrettyPrinting().create();
+        
+        ArrayList<Product> results=new ArrayList<Product>();
+		
+        try {
+			results = page.genProductList();
+		} catch (IOException e) {
+			System.out.println("cannot connect to url");
+		}
+        
+        double grossTotal=0;
+        
+        for(Product p : results) {
+        	grossTotal += Double.parseDouble(p.getUnit_price());
+        }
+        Total total = new Total(grossTotal);       
+        
+        JsonObj json = new JsonObj(results, total);
+        
+        System.out.println(gson.toJson(json));  
 	}
 
 }
