38c3c9b1cf8e2872cb0f1eb0a7685f568f4c30fc
Ben Drazen <ben.drazen@gmail.com>
2019-05-05 17:02:01 +0100

    gson builder added
    gson builder added and product list serialised

--- src/org/bend/scraper/SainsburysScraper.java
+++ src/org/bend/scraper/SainsburysScraper.java
@@ -1,10 +1,20 @@
 package org.bend.scraper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class SainsburysScraper {
 //main class for the app. 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Page page = new Page ("https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html","productLister gridView");
+		page.init();
+		GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        
+        System.out.println(gson.toJson(page.getProductList()));
+         
 		
 	}
 
